cef4fed479abeddf6f38094675fb782627206db6
jnchen <caojingchen@live.com>
2015-08-29 00:11:25 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,7 +118,7 @@ function afterPjax() {
 
 // Lazy Loading Disqus
 //   //   // http://jsfiddle.net/dragoncrew/SHGwe/1/
-
+/*
    var ds_loaded = false,
       top = $('#disqus_thread').offset().top;
   window.disqus_shortname = $('#disqus_thread').attr('name');
@@ -135,4 +135,5 @@ function afterPjax() {
     }
   }check();
   container.scroll(check);
+  */
 }afterPjax();
