50a41704e533c99e9454c79d6a54cbca8ab2cd53
jnchen <caojingchen@live.com>
2015-08-30 12:21:19 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,21 +119,20 @@ function afterPjax() {
   // Lazy Loading Disqus
   // http://jsfiddle.net/dragoncrew/SHGwe/1/
   var ds_loaded = false,
-      top = $('#ds-thread').offset().top;
-  
+      top = $('#disqus_thread').offset().top;
+  window.disqus_shortname = $('#disqus_thread').attr('name');
 
   function check() {
     if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
       $.ajax({
         type: 'GET',
-        url: (document.location.protocol == 'https:' ? 'https:' : 'http:')+'//static.duoshuo.com/embed.js',
+        url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
         dataType: 'script',
-        cache: false
+        cache: true
       });
-      ds_loaded = false;
+      ds_loaded = true;
     }
   }check();
-
   container.scroll(check);
 }afterPjax();
 
